fdac2f04c35ca26ef256f5ad07486435a8936db0
Kenneth Carroll <kenneth.james.carroll@gmail.com>
2024-02-21 17:51:56 -0500

    Update UI components and introduce volume control
    refactors menu buttons  A new `volume_slider` widget has been created to control the volume within the app rather than being separate.

--- lib/widgets/menu_button.dart
+++ lib/widgets/menu_button.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:kolor_klash/styles/background_gradient.dart';
-import 'package:kolor_klash/widgets/white_menu_text.dart';
+import 'package:kolor_klash/widgets/gradient_text.dart';
 
 class MenuButton extends StatelessWidget {
   final String buttonText;
   final Function onPressed;
+  final double? height;
 
-  const MenuButton({super.key, required this.buttonText, required this.onPressed});
+  const MenuButton({super.key, required this.buttonText, required this.onPressed, this.height});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialButton(
-      color: Colors.white,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ShaderMask(
-              shaderCallback: (bounds) => backgroundGradient.createShader(bounds),
-              child: WhiteStyledText(text: buttonText)
-          )
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: MaterialButton(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15)
+        ),
+        child: Container(
+          constraints: BoxConstraints(
+            minHeight: height ?? 50
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GradientText(buttonText: buttonText)
+            ],
+          ),
+        ),
+        onPressed: () => onPressed(),
       ),
-      onPressed: () => onPressed(),
     );
   }
 }
