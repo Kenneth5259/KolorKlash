1730189c5c833dee77bbb1bbe84b5ac3f376a23d
Ken <kenneth.james.carroll@gmail.com>
2024-09-28 16:44:50 -0400

    Scoreboard color and sort icon

--- lib/screens/score_board_screen.dart
+++ lib/screens/score_board_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../state/subclasses/scoreboard.dart';
 
 class ScoreBoardScreen extends StatefulWidget {
@@ -8,57 +7,72 @@ class ScoreBoardScreen extends StatefulWidget {
   const ScoreBoardScreen({Key? key, required this.scores}) : super(key: key);
 
   @override
-  State<ScoreBoardScreen> createState() => _ScoreBoardScreenState();
+  ScoreBoardScreenState createState() => ScoreBoardScreenState();
 }
 
-class _ScoreBoardScreenState extends State<ScoreBoardScreen> {
-  late List<ScoreEntry> _scores;
+class ScoreBoardScreenState extends State<ScoreBoardScreen> {
+  List<ScoreEntry> _scores;
   bool _ascending = true;
+  int _sortColumnIndex = 0;
+
+  ScoreBoardScreenState() : _scores = [];
 
   @override
   void initState() {
     super.initState();
-    _scores = widget.scores;
+    _scores.addAll(widget.scores);
   }
 
-  void _sort<T>(T Function(ScoreEntry s) getField, int Function(T a, T b) compare, bool ascending) {
-    _scores.sort((ScoreEntry a, ScoreEntry b) {
+  void _sort<T>(int columnIndex, T Function(ScoreEntry s) getField, bool ascending) {
+    _scores = List.from(_scores)..sort((ScoreEntry a, ScoreEntry b) {
       final aValue = getField(a);
       final bValue = getField(b);
       return ascending
-          ? compare(aValue, bValue)
-          : compare(bValue, aValue);
+          ? Comparable.compare(aValue as Comparable, bValue as Comparable)
+          : Comparable.compare(bValue as Comparable, aValue as Comparable);
     });
     setState(() {
+      _sortColumnIndex = columnIndex;
       _ascending = ascending;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Row(
       children: [
         Column(
           children: [
-            DataTable(
-              sortColumnIndex: 0,
-              sortAscending: _ascending,
-              columns: [
-                DataColumn(label: const Text('Date'),
-                    onSort: (int columnIndex, bool ascending) => _sort<DateTime>((ScoreEntry d) => d.scoreDate, (a, b) => a.compareTo(b), ascending)),
-                DataColumn(label: const Text('Score'),
-                    onSort: (int columnIndex, bool ascending) => _sort<int>((ScoreEntry s) => s.scoreValue, (a, b) => a.compareTo(b), ascending)),
-                DataColumn(label: const Text('Turns'),
-                    onSort: (int columnIndex, bool ascending) => _sort<int>((ScoreEntry t) => t.turnCount, (a, b) => a.compareTo(b), ascending)),
-              ],
-              rows: _scores.map((ScoreEntry scoreEntry) {
-                return DataRow(cells: [
-                  DataCell(Text('${scoreEntry.scoreDate}')),
-                  DataCell(Text('${scoreEntry.scoreValue}')),
-                  DataCell(Text('${scoreEntry.turnCount}')),
-                ]);
-              }).toList(),
+            IconTheme(
+              data: const IconThemeData(color: Colors.white),
+              child: DataTable(
+                sortColumnIndex: _sortColumnIndex,
+                sortAscending: _ascending,
+                columns: [
+                  DataColumn(
+                    label: const Text('Date', style: TextStyle(color: Colors.white, fontSize: 20)),
+                    onSort: (columnIndex, ascending) =>
+                        _sort<DateTime>(columnIndex, (ScoreEntry d) => d.scoreDate, ascending),
+                  ),
+                  DataColumn(
+                    label: const Text('Score', style: TextStyle(color: Colors.white, fontSize: 20)),
+                    onSort: (columnIndex, ascending) =>
+                        _sort<int>(columnIndex, (ScoreEntry s) => s.scoreValue, ascending),
+                  ),
+                  DataColumn(
+                    label: const Text('Turns', style: TextStyle(color: Colors.white, fontSize: 20)),
+                    onSort: (columnIndex, ascending) =>
+                        _sort<int>(columnIndex, (ScoreEntry t) => t.turnCount, ascending),
+                  ),
+                ],
+                rows: _scores.map((ScoreEntry scoreEntry) {
+                  return DataRow(cells: [
+                    DataCell(Text('${scoreEntry.scoreDate}')),
+                    DataCell(Text('${scoreEntry.scoreValue}')),
+                    DataCell(Text('${scoreEntry.turnCount}')),
+                  ]);
+                }).toList(),
+              ),
             ),
           ],
         ),
